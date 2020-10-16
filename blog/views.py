b9664c7b0586ffde34db63895f0ec8786666fe37
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-10-16 10:28:22 +0100

    manage url in base.html

--- blog/views.py
+++ blog/views.py
@@ -44,3 +44,7 @@ def home(request) :
         'posts' : posts
     }
     return render(request,'blog/index.html',context)
+
+def about(request) :
+
+    return render(request,'blog/about.html', {'title' : 'من انا'})
