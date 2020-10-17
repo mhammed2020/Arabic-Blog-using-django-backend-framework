2ba2f7c96aebfc3a9f8cf48ff2f0e8f3ecbbadac
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-10-17 19:36:22 +0100

    create latest_comments.html to render comment

--- blog/views.py
+++ blog/views.py
@@ -24,11 +24,23 @@ def post_detail(request,post_id) :
     comments = post.comments.filter(active=True)
 
     comment_form = NewComment()
-
+    new_comment = None
     context ={
         'title':post,
         'post' : post,
         'comments':comments,
         'comment_form' :comment_form, 
     }
+
+    if request.method == 'POST' :
+        comment_form = NewComment(data = request.POST)
+
+        if comment_form.is_valid() :
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+            comment_form = NewComment()
+
+    else :
+        comment_form = NewComment()
     return render(request,'blog/detail.html', context)
