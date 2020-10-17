44cb561d56c97b8976486e25215b008b9f9938e0
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-10-17 16:42:39 +0100

    create forms.py for Comment  class

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,get_object_or_404
 
 from . models import Post,Comment
 
+from .forms import NewComment
 # Create your views here.
 
 def home(request) :
@@ -22,9 +23,12 @@ def post_detail(request,post_id) :
     post = get_object_or_404(Post,pk=post_id)
     comments = post.comments.filter(active=True)
 
+    comment_form = NewComment()
+
     context ={
         'title':post,
         'post' : post,
-        'comments':comments
+        'comments':comments,
+        'comment_form' :comment_form, 
     }
     return render(request,'blog/detail.html', context)
