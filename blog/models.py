e8a12b2e4cfc747807553461c44aafe86d1c99cb
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-10-17 15:56:53 +0100

    customize comment in detail.html and main.css

--- blog/models.py
+++ blog/models.py
@@ -16,5 +16,22 @@ class Post(models.Model):
     class Meta :
         ordering = ('-post_date',)
 
+
+class Comment(models.Model):
+
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    body = models.TextField()
+    comment_date = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     
 
+    def __str__(self):
+        return ' علق {} على {} '.format(self.name,self.post)
+
+    
+
+    class Meta :
+        ordering = ('-comment_date',)
