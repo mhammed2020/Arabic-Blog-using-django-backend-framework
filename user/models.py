17973ea59c6853d270936d7351b010332938df86
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-10-21 13:24:29 +0100

    Analyse site performane ..resize images with pillow library

--- user/models.py
+++ user/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+
+# manage image size
+from PIL import Image
 # Create your models here.
 class Profile(models.Model):
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
@@ -8,6 +11,8 @@ class Profile(models.Model):
 
     def __str__(self):
         return '{} profile.'.format(self.user.username)
+
+    
 
 def create_profile(sender, **kwarg) :
     if kwarg['created'] :
